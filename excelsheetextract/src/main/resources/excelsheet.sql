aba73ef8deb0cc2d865c50f4fd4b9fc093ba3760
loveguru <gpgurun1992@gmail.com>
2017-12-31 12:33:28 +0530

    sql

---
+++ excelsheetextract/src/main/resources/excelsheet.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.1.42-community : Database - testexcel
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`testexcel` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `testexcel`;
+
+/*Table structure for table `excelsheetinfo` */
+
+DROP TABLE IF EXISTS `excelsheetinfo`;
+
+CREATE TABLE `excelsheetinfo` (
+  `excelsheet_Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `company` varchar(40) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `salary` double DEFAULT NULL,
+  PRIMARY KEY (`excelsheet_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `excelsheetinfo` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
